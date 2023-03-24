a44626171c1d8a0af8c59c6c722e95f33b5799ad
Ajdavidl <78770941+ajdavidl@users.noreply.github.com>
2023-03-24 17:42:28 -0300

    create function content to show the data.

--- src/Multilingual.jl
+++ src/Multilingual.jl
@@ -2,6 +2,21 @@ module Multilingual
 
 import YAML
 
-greet() = print("Hello World!")
+export content
+
+function content(language::AbstractString, category::AbstractString)
+    @assert language in ["pt", "en"]
+    filename = joinpath(@__DIR__, "..", "data", language * "-content.yml")
+    data = YAML.load_file(filename)
+    Keys = keys(data)
+    if category in Keys
+        for (i, j) in data[category]
+            println(i, " => ", j)
+        end
+    else
+        println("Sorry, category " * category * " does not exist.")
+    end
+end
+
 
 end # module Multilingual
