54176a98c4caa39cb8ded9356c6a1528a9d412ee
Ajdavidl <78770941+ajdavidl@users.noreply.github.com>
2023-07-06 15:39:02 -0300

    Add document strings to the functions.

--- src/Multilingual.jl
+++ src/Multilingual.jl
@@ -6,11 +6,44 @@ const global LANGUAGES = ["pt", "en", "es", "it", "fr", "de", "ro", "ca"]
 
 export content, readData, compare
 
+"""
+
+    readData(language::AbstractString)
+
+Reads the YAML file with all words and categories for the specified language.
+
+# Argument
+- `language::AbstractString` : the target language code. Examples: "en", "es", "pt", "it", "fr", "ca", "ro", "de"
+
+
+# Example
+```julia-repl
+julia> fr = readData("fr");
+```
+"""
 function readData(language::AbstractString)
     filename = joinpath(@__DIR__, "..", "data", language * "-content.yml")
     return YAML.load_file(filename)
 end
 
+"""
+
+    content(language::AbstractString, category::AbstractString)
+
+Prints the words of a category in specified language.
+
+# Arguments
+- `language::AbstractString` : the target language code. Examples: "en", "es", "pt", "it", "fr", "ca", "ro", "de"
+- `category::AbstractString` : the category of the words. Examples: colors, family, week, greetings and others.
+
+
+# Examples
+```julia-repl
+julia> content("en","greetings")
+hello => ["hello", "hi"]
+welcome => welcome
+```
+"""
 function content(language::AbstractString, category::AbstractString)
     @assert language in LANGUAGES
     data = readData(language)
@@ -25,6 +58,30 @@ function content(language::AbstractString, category::AbstractString)
     end
 end
 
+
+"""
+
+    compare(language1::AbstractString, language2::AbstractString, category::AbstractString)
+
+Prints the words of a category in two specified languages. It compares the words in the two languages.
+
+# Arguments
+- `language1::AbstractString` : the language code. Examples: "en", "es", "pt", "it", "fr", "ca", "ro", "de"
+- `language2::AbstractString` : the language code. Examples: "en", "es", "pt", "it", "fr", "ca", "ro", "de"
+- `category::AbstractString` : the category of the words. Examples: colors, family, week, greetings and others.
+
+
+# Examples
+```julia-repl
+julia> compare("pt","es","greetings")
+hello :
+        pt => ["olá", "oi"]
+        es => hola
+welcome :
+        pt => ["bem-vindo", "bem-vinda", "bem-vindos", "bem-vindas"]
+        es => ["bienvenido", "bienvenidos"]
+```
+"""
 function compare(language1::AbstractString, language2::AbstractString, category::AbstractString)
     @assert language1 in LANGUAGES
     @assert language2 in LANGUAGES
